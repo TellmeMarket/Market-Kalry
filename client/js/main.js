1c31e4b3b647282c609426946f225c2e36aeb746
KyoungJu Lee <qurtks2224@gmail.com>
2023-02-03 15:23:42 +0900

    [feat] 메인 상단배너 삭제  기능 추가

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,4 @@
-import { getNode, getNodes } from "/client/lib/index.js";
+import { getNode } from "/client/lib/index.js";
 
 // header sticky!
 const $nav = getNode(".header-nav");
@@ -8,6 +8,18 @@ const navHandler = (_) => {
   else [...$nav.children].forEach((el) => el.classList.remove("active"));
 };
 addEventListener("scroll", navHandler);
+
+// hideBanner Part
+const $bannder = getNode(".top-banner");
+localStorage.getItem("hide-banner") ? ($bannder.style.display = "none") : $bannder.classList.add("active");
+
+const bannerHide = (e) => {
+  e.preventDefault();
+  !localStorage.getItem("hide-banner") && e.target.closest(".close-button") && $bannder.classList.remove("active");
+  localStorage.setItem("hide-banner", "hide");
+};
+addEventListener("scroll", navHandler);
+$bannder.addEventListener("click", bannerHide);
 
 // main-banner swiper
 const swiper1 = new Swiper(".swiper-1", {
