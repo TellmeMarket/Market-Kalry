e66ec8e6c977c88a3952c51fe058192f257ba4ac
hyungjin kim <hjux07@gmail.com>
2023-02-04 01:47:51 +0900

    [feat] 체크함수 객체화, input이벤트 위임적용, 전체동의기능 완성

--- client/components/signup/js/signin.js
+++ client/components/signup/js/signin.js
@@ -3,15 +3,15 @@ const signupFunc = (() => {
   const $signBtn = $form.querySelector(".signBtn");
   const $popup = document.querySelector(".popup");
 
-  return () => {
-    // 에러 여부에따라 확인 후 에러 제거함수
-    const clearErr = (node) => {
+
+     // 에러 여부에따라 확인 후 에러 제거함수
+    const clearErr = node => {
       const errMsgs = node.closest("td").querySelector("p");
       errMsgs && node.closest("td").querySelector("p").remove();
     };
 
     // 팝업창
-    const popup = (text) => {
+    const popup = text => {
       if (!$popup.classList.contains("active")) {
         $popup.querySelector("p").textContent = text;
         document.body.classList.add("off");
@@ -21,59 +21,65 @@ const signupFunc = (() => {
         $popup.classList.remove("active");
       }
     };
- 
-    // userId
-    const isTxt = (id, minLen, maxLen) => {
-      // 정규식표현
-      const idValue = id.value.trim();
-      const template = `<p>${minLen}자 이상 ${maxLen}자 이하의 영문 혹은 영문과 숫자를 조합</p>`;
-      clearErr(id);
-      if (idValue.length >= minLen && idValue.length <= maxLen) console.log(true);
-      else if (idValue.length >= 1) id.closest("td").insertAdjacentHTML("beforeend", template);
-      else if (idValue.length === 0) clearErr(id);
-    };
 
-    // passWord
-    const isPwd = (pwd) => {
-      const regPass = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{10}$/;
-      const template = "wafwe";
-      console.log(regPass.test(pwd.value));
-      clearErr(pwd);
-      regPass.test(pwd.value) || pwd.closest("td").insertAdjacentHTML("beforeend", template);
-      pwd.value.length === 0 && clearErr(pwd);
-    };
+  const isChecked = {
+        isTxt (id, minLen, maxLen)  {
+          const idValue = id.value.trim();
+          const template = `<p>${minLen}자 이상 ${maxLen}자 이하의 영문 혹은 영문과 숫자를 조합</p>`;
+          clearErr(id);
+          if (idValue.length >= minLen && idValue.length <= maxLen) console.log(true);
+          else if (idValue.length >= 1) id.closest("td").insertAdjacentHTML("beforeend", template);
+          else if (idValue.length === 0) clearErr(id);
+        },
+        isPwd(pwd) {
+          const regPass = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{10}$/;
+          const template = "wafwe";
+          console.log(regPass.test(pwd.value));
+          clearErr(pwd);
+          regPass.test(pwd.value) || pwd.closest("td").insertAdjacentHTML("beforeend", template);
+          pwd.value.length === 0 && clearErr(pwd);
+        },
+        isPwdCheck(pwd, minLen) {
+          const regPass = "^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{10}$";
+        // if(regPass.test(pwd.))
+        },
+        isName(){
+          return 11
+        },
+        isEmail(){
+        },
+        isPhoneNumber(){
 
-    const isPwdCheck = (pwd, minLen) => {
-      const regPass = "^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{10}$";
+        }
+    }
+    const { isTxt, isPwd , isPwdCheck, isName, isEmail, isPhoneNumber } = isChecked;
 
-      // if(regPass.test(pwd.))
-    };
-    const isName = () => {};
-    const isEmail = () => {};
-    const isPhoneNumber = () => {};
+  return () => {
 
     // inputEvent
-    $form.querySelectorAll(".input").forEach((el) => {
-      el.addEventListener("input", (e) => {
-        // console.log(e.currentTarget.value);
-        e.target.id === "userid" && isTxt(e.target, 6, 16);
-        e.target.id === "pwd" && isPwd(e.target);
-        e.target.id === "pwdCheck" && isPwdCheck(e.target, 10);
-        e.target.id === "name" && isName(e.target, 10);
-        e.target.id === "email" && isEmail(e.target, 10);
-        e.target.id === "phonNumber" && isPhoneNumber(e.target, 10);
-      });
-    });
+    const inputHandler = e =>{      
+      e.target.id === "userid" && isTxt(e.target, 6, 16);
+      e.target.id === "pwd" && isPwd(e.target);
+      e.target.id === "pwdCheck" && isPwdCheck(e.target, 10);
+      e.target.id === "name" && isName(e.target, 10);
+      e.target.id === "email" && isEmail(e.target, 10);
+      e.target.id === "phonNumber" && isPhoneNumber(e.target, 10);
+    }
 
-    // 전체동의 체크
-    const allCheck = (target, bool) => {
-      target
-        .closest(".column")
-        .querySelectorAll("input[type=checkbox]")
-        .forEach((el) => (el.checked = bool));
-    };
+     // 전체동의 체크 2
+     const allCheck = (target, bool) => target.closest(".column").querySelectorAll("input[type=checkbox]").forEach((el) => (el.checked = bool));
 
-    const clickHandler = (e) => {
+     const clickHandler = e => {
+       
+      // 전체동의 체크 1
+      if(e.target.dataset.check === 'check'){
+        e.target.name === 'agreeAll'&& allCheck(e.target, e.target.checked);
+        const result = [...document.querySelectorAll('.required')].filter(el=>el.checked === true);
+        result.length === 4 && allCheck(e.target, true)
+      }
+
+
+
       // userId 중복확인 + 팝업
       // 1. userId 중복확인버튼을 눌렀을때 input.vaue가 해당 조건이 아니면 팝업창을 띄움
       // 2. userId 중복확인버튼을 눌렀을때 input.vaue가 해당 조건이 맞으면 팝업창을 띄우고 중복확인 버튼 off
@@ -83,11 +89,7 @@ const signupFunc = (() => {
       // 2. 이메일 형식이 아닐경우 이메일 형식입력하라는 팝업창 띄움
       // 3. 이메일 형식이 맞을 경우 사용가능한 메일이라는 팝업창 뜨우고 버튼 off
 
-      // 전체동의
-      if (e.target.id === "agreeAll") {
-        if (e.target.checked === true) allCheck(e.target, true);
-        else allCheck(e.target, false);
-      }
+
 
       // 가입하기 버튼
       // 1. userId 중복확인 버튼이 off인가 ? 아니면 return하면서 "아이디 중복체크해주세요"팝업창출력
@@ -99,11 +101,13 @@ const signupFunc = (() => {
       // 7. 약관동의 1,2,3에 체크가 되어있는가 ? 아니면 return하면서 "아이디 중복체크해주세요"팝업창출력
     };
 
+    // eventList
+    $form.addEventListener('input',inputHandler)
     $form.addEventListener("click", clickHandler);
   };
 })();
-signupFunc();
 
+signupFunc();
 // const clickHandler = (e) => {
 //   e.preventDefault();
 //   e.target.tagName === "BUTTON" && popup("text");
