b323c2c40c3ea63ab0594e5c543fa5aca7bb2e5a
leejh4197 <leejh4197@naver.com>
2023-02-08 02:45:10 +0900

    [feat]장바구니 데이터뿌려주기

--- client/js/cart.js
+++ client/js/cart.js
@@ -1,20 +1,20 @@
 const arrowIcon = document.querySelector(".arrow");
 const product = document.querySelector(".food-cate");
 const foodWrap = document.querySelector(".food-wrap");
-const defaultStyle = getComputedStyle(foodWrap).borderBottom;
 const upIcon = "http://localhost:5500/assets/icons/Icon/Direction=Up.svg";
 const downIcon = "http://localhost:5500/assets/icons/Icon/Direction=Down.svg";
+const cartList = JSON.parse(localStorage.getItem("cart-product"));
 
-console.log(defaultStyle);
 const activeArrowIcon = () => {
   const iconToggle = arrowIcon.classList.toggle("active");
 
-  template = /* html */ `
+  cartList.map((el) => {
+    template = /* html */ `
   <div class="cart-add-product">
                 <div class="cart-add-product-title">
                   <img src="../../assets/icons/Icon/isChecked=false.svg" alt="체크아이콘" />
-                  <img src="../../assets/tangtang/detail_view.jpg" alt="" />
-                  <span>[풀무원] 탱탱쫄면 (4개입)</span>
+                  <img src="../.${el.cartImage}" alt="" />
+                  <span>${el.name}</span>
                 </div>
                 <div class="cart-quantity-wrap">
                   <div class="cart-quantity">
@@ -26,19 +26,21 @@ const activeArrowIcon = () => {
                       <span> &#43;</span>
                     </button>
                   </div>
-                  <span>4,980원</span>
+                  <span>${el.price}</span>
                   <img src="../../assets/header/top-banner-icon.svg" alt="" />
                 </div>
               </div>
   `;
-  if (iconToggle === true) {
-    arrowIcon.src = upIcon;
-    product.insertAdjacentHTML("beforeend", template);
-  } else {
-    const close = document.querySelector(".cart-add-product");
-    arrowIcon.src = downIcon;
-    close.remove();
-  }
+
+    if (iconToggle === true) {
+      arrowIcon.src = upIcon;
+      product.insertAdjacentHTML("beforeend", template);
+    } else {
+      const close = document.querySelector(".cart-add-product");
+      arrowIcon.src = downIcon;
+      close.remove();
+    }
+  });
 };
 
 arrowIcon.addEventListener("click", activeArrowIcon);
